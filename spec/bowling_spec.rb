3aaf12ead7eddd0173623182d0e802b21b0d3647
Daniel <keendaniel01@gmail.com>
2018-01-19 13:07:48 +0000

    add lib and spec directories

---
+++ spec/bowling_spec.rb
@@ -0,0 +1,21 @@
+require 'bowling'
+
+RSpec.describe Bowling, "#score" do
+  context "with no strikes or spares" do
+    it "sums the pin count for each roll" do
+      bowling = Bowling.new
+      20.times { bowling.hit(4) }
+      expect(bowling.score).to eq 80
+
+      20.times { bowling.hit(4) }
+      expect(bowling.score).to eq 160
+
+    end
+
+    it "sums the pin count again" do
+      bowling_two = Bowling.new
+      10.times { bowling_two.hit(2) }
+      expect(bowling_two.score).to eq 20
+    end
+  end
+end
